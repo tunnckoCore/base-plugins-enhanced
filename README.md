b53f76fc29e73a8aab1077a9c3b00b7294ca0420
tunnckoCore <mameto_100@mail.bg>
2016-08-13 06:10:56 +0300

    update verb docs

--- README.md
+++ README.md
@@ -16,6 +16,95 @@ npm i base-plugins-enhanced --save
 const basePluginsEnhanced = require('base-plugins-enhanced')
 ```
 
+### [basePluginsEnhanced](index.js#L41)
+> Upgrade the built-in plugin system or that comes from [base-plugins][] to have error handling and some little extra stuff. Like you can pass array of plugins to `.use` method and also `options` object as second argument.
+
+**Params**
+
+* `options` **{Object}**: object merge with `app.options`    
+* `returns` **{Function}**: plugin executed by `.use` method  
+
+**Example**
+
+```js
+var plugins = require('base-plugins-enhanced')
+var Base = require('base')
+var app = new Base()
+
+app
+  .use(plugins())
+  .use(function foo () {}, { aa: 'bb' })
+  .use(function bar () {}, { xx: 'yy' })
+  .use([
+    function pluginOne () {},
+    function pluginTwo () {},
+  ], opts)
+```
+
+### [.use](index.js#L67)
+> Add plugin to your [base][] application. See Base's or the [base-plugins][] documentation.
+
+**Params**
+
+* `fn` **{Function|Array}**: function or array of functions    
+* `opts` **{Object}**: options to be merged with `app.options`    
+* `returns` **{Base}**: instance for chaining  
+
+**Example**
+
+```js
+app.use([
+  function one () {},
+  function two () {}
+], { foo: 'bar' })
+```
+
+### [.run](index.js#L142)
+> Run the stack of plugins. See more on [base-plugins][] documentation on `.run` method.
+
+**Params**
+
+* `val` **{Object}**: object to be passed to each smart plugin    
+* `returns` **{Base}**: instance for chaining  
+
+**Example**
+
+```js
+app
+  .use(function foo (app) {
+    return function (ctx) {
+      ctx.foo = 'fooooo'
+    }
+  }, { first: 'yes' })
+  .use([
+    function bar (app) {
+      return function (ctx) {
+        ctx.bar = 'barrr'
+      }
+    }
+    function baz (app) {
+      return function (ctx) {
+        ctx.qux = 123
+      }
+    }
+  ], { multiple: true })
+  .use(function qux (app) {
+    app.zzz = 'yyyy'
+  }, { aaa: bbb })
+
+var obj = {a: 'b'}
+app.run(obj)
+
+console.log(app.zzz)
+// => 'yyyy'
+
+console.log(obj)
+// => { foo: 'fooooo', bar: 'barrr', qux: 123 }
+
+console.log(app.options)
+// => { first: 'yes', multiple: true, aaa: 'bbb', }
+```
+
 ## Contributing
 Pull requests and stars are always welcome. For bugs and feature requests, [please create an issue](https://github.com/tunnckoCore/base-plugins-enhanced/issues/new).  
 But before doing anything, please read the [CONTRIBUTING.md](./CONTRIBUTING.md) guidelines.
@@ -23,6 +112,9 @@ But before doing anything, please read the [CONTRIBUTING.md](./CONTRIBUTING.md) 
 ## [Charlike Make Reagent](http://j.mp/1stW47C) [![new message to charlike][new-message-img]][new-message-url] [![freenode #charlike][freenode-img]][freenode-url]
 
 [![tunnckoCore.tk][author-www-img]][author-www-url] [![keybase tunnckoCore][keybase-img]][keybase-url] [![tunnckoCore npm][author-npm-img]][author-npm-url] [![tunnckoCore twitter][author-twitter-img]][author-twitter-url] [![tunnckoCore github][author-github-img]][author-github-url]
+
+[base-plugins]: https://github.com/node-base/base-plugins
+[base]: https://github.com/node-base/base
 
 [npmjs-url]: https://www.npmjs.com/package/base-plugins-enhanced
 [npmjs-img]: https://img.shields.io/npm/v/base-plugins-enhanced.svg?label=base-plugins-enhanced
--- index.js
+++ index.js
@@ -9,12 +9,61 @@
 
 var utils = require('./utils')
 
+/**
+ * > Upgrade the built-in plugin system or that
+ * comes from [base-plugins][] to have error handling
+ * and some little extra stuff. Like you can pass array
+ * of plugins to `.use` method and also `options` object
+ * as second argument.
+ *
+ * **Example**
+ *
+ * ```js
+ * var plugins = require('base-plugins-enhanced')
+ * var Base = require('base')
+ * var app = new Base()
+ *
+ * app
+ *   .use(plugins())
+ *   .use(function foo () {}, { aa: 'bb' })
+ *   .use(function bar () {}, { xx: 'yy' })
+ *   .use([
+ *     function pluginOne () {},
+ *     function pluginTwo () {},
+ *   ], opts)
+ * ```
+ *
+ * @param  {Object} `options` object merge with `app.options`
+ * @return {Function} plugin executed by `.use` method
+ * @api public
+ */
 module.exports = function basePluginsEnhanced (options) {
   return function enhancedUseMethod (app) {
     if (app.isRegistered('base-plugins-enhanced')) return
 
     var originalUse = app.use.bind(app)
     app.options = utils.extend({}, app.options, options)
+
+    /**
+     * > Add plugin to your [base][] application. See Base's or
+     * the [base-plugins][] documentation.
+     *
+     * **Example**
+     *
+     * ```js
+     * app.use([
+     *   function one () {},
+     *   function two () {}
+     * ], { foo: 'bar' })
+     * ```
+     *
+     * @name   .use
+     * @param  {Function|Array} `fn` function or array of functions
+     * @param  {Object} `opts` options to be merged with `app.options`
+     * @return {Base} instance for chaining
+     * @api public
+     */
+
     app.define('use', function use (fn, opts) {
       if (utils.isObject(opts)) {
         app.options = utils.extend({}, app.options, opts)
@@ -41,6 +90,55 @@ module.exports = function basePluginsEnhanced (options) {
 
     if (app.run) {
       var originalRun = app.run.bind(app)
+
+      /**
+       * > Run the stack of plugins. See more on [base-plugins][]
+       * documentation on `.run` method.
+       *
+       * **Example**
+       *
+       * ```js
+       * app
+       *   .use(function foo (app) {
+       *     return function (ctx) {
+       *       ctx.foo = 'fooooo'
+       *     }
+       *   }, { first: 'yes' })
+       *   .use([
+       *     function bar (app) {
+       *       return function (ctx) {
+       *         ctx.bar = 'barrr'
+       *       }
+       *     }
+       *     function baz (app) {
+       *       return function (ctx) {
+       *         ctx.qux = 123
+       *       }
+       *     }
+       *   ], { multiple: true })
+       *   .use(function qux (app) {
+       *     app.zzz = 'yyyy'
+       *   }, { aaa: bbb })
+       *
+       * var obj = {a: 'b'}
+       * app.run(obj)
+       *
+       * console.log(app.zzz)
+       * // => 'yyyy'
+       *
+       * console.log(obj)
+       * // => { foo: 'fooooo', bar: 'barrr', qux: 123 }
+       *
+       * console.log(app.options)
+       * // => { first: 'yes', multiple: true, aaa: 'bbb', }
+       * ```
+       *
+       * @name   .run
+       * @param  {Object} `val` object to be passed to each smart plugin
+       * @return {Base} instance for chaining
+       * @api public
+       */
+
       app.define('run', function run (val) {
         try {
           originalRun(val)
